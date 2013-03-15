1009660207bd480fb7bc188e94fdf4a58c7a3188
Obijuan <juan@iearobotics.com>
2013-03-15 12:31:17 +0100

    compilation command added for read_config utility

--- Software/mcp2200-utils/compile.sh
+++ Software/mcp2200-utils/compile.sh
@@ -2,7 +2,10 @@
 
 echo "compiling..."
 
+#-- Reading the config settings of the mcp2200 chip
+gcc mcp2200.c read_config.c  -I/usr/include/libusb-1.0 -I. -lusb-1.0 -lrt -lpthread  -o read_config
 
+#-- Configuring the mcp2200 chip for the DCON board
 gcc mcp2200.c mcp2200-config.c -I/usr/include/libusb-1.0 -I. -lusb-1.0 -lrt -lpthread  -o mcp2200-config
 
 
